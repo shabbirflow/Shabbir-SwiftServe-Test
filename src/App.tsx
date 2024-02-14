cfaff94a6a28c5db0e0046cdf2f46dac975915ed
shabbir <kaderishabbir@gmail.com>
2024-02-14 21:17:26 +0530

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import shrekImage from './assets/shrek.webp'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
